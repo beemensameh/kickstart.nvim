6ca6d01cb51fb8dd7f702887e24a5f1273012e41
beemensameh <41084077+beemensameh@users.noreply.github.com>
2024-06-15 21:07:19 +0300

    split auto commands

--- lua/kickstart/init.lua
+++ lua/kickstart/init.lua
@@ -1,11 +1,5 @@
 require('kickstart.options')
-
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
+require('kickstart.autocommands')
 require('kickstart.plugins')
+
+-- vim: ts=2 sts=2 sw=2 et
