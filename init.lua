b23d57016594ef938d09fa299688108293e2e6b2
beemensameh <41084077+beemensameh@users.noreply.github.com>
2025-05-04 22:09:38 +0300

    chore: add install node url

--- init.lua
+++ init.lua
@@ -405,6 +405,7 @@ require('lazy').setup({
         --     },
         --   },
         -- },
+        -- -- Instal node https://nodejs.org/en/download/
         -- pyright = {
         --   filetypes = { "python" },
         --   settings = {
